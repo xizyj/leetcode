ecf6b7836f9591099e5b298c1b6f44e2f669c180
zhouyijun <1176902387@qq.com>
2019-09-24 19:50:32 +0800

    init

--- src/Solution.java
+++ src/Solution.java
@@ -5,14 +5,27 @@ public class Solution {
     public static void main(String args[]){
         Scanner scanner=new Scanner(System.in);
         String str=scanner.nextLine();
+        System.out.println(func("ciso<1234>"));
     }
 
-    public static boolean func(String str){
-        if(str.charAt(0)==str.charAt(4)&&str.charAt(1)==str.charAt(3)){
-            return true;
+    public static String func(String str){
+        int start=0;
+        int end=0;
+        int flag=0;
+        for(int i=0;i<str.length();i++){
+            if((str.charAt(i)>='A'&&str.charAt(i)<='Z'||str.charAt(i)>='a'&&str.charAt(i)<='z')&&flag==0){
+                start=i;
+                flag=1;
+            }
+            if((str.charAt(i)>='A'&&str.charAt(i)<='Z'||str.charAt(i)>='a'&&str.charAt(i)<='z')&&flag==1){
+                end=i;
+            }
+            if(str.charAt(i)=='<'){
+                break;
+            }
         }
-        else
-            return false;
+        return str.substring(start,end+1);
+
     }
 
 
